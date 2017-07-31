117012e72ba47bf611705125847fffdf32abfeea
Viktor <ctxx61@gmail.com>
2017-07-31 19:47:14 +0800

    Add GameOverUI and NextLevel UI Add ClickSound and BGM

---
+++ DontStop/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour {
+    public GameObject sceneManager;
+	// Use this for initialization
+	void Start () {
+        sceneManager = GameObject.FindGameObjectWithTag("SceneManager");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    public void OnScene(string i)
+    {
+        sceneManager.GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene(i);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        sceneManager.GetComponent<AudioSource>().Play();
+    }
+}
