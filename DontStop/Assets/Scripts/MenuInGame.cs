96e15ad7e76a87bc88403f078217f4c82a6e6cca
陈铁 <ctxx61@gmail.com>
2017-09-25 15:11:22 +0100

    try to merge

--- DontStop/Assets/Scripts/MenuInGame.cs
+++ DontStop/Assets/Scripts/MenuInGame.cs
@@ -6,15 +6,16 @@ public class MenuInGame : MonoBehaviour {
     public GameObject menu;
     public bool showMenu = false;
     public GameObject player;
+    
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         //player.GetComponent<PlayerControl>().enabled = true;
         //Time.timeScale = 1;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		if (Input.GetKeyDown(KeyCode.Escape))
         {
             Time.timeScale = 0;
